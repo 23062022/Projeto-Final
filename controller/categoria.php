9cab88c76fcadb1c504887688c8b9a44f90c11c9
Nathaly Terezinha de Alvarenga <14541536609@DC01.IFS.IFSULDEMINAS.EDU.BR>
2022-06-23 09:09:45 -0300

    projeto inicial

---
+++ controller/categoria.php
@@ -0,0 +1,9 @@
+<?php
+require "../model/categoriaModel.php";
+
+$categoria =  new categoriaModel();
+$categoria->inserir("SmartTV");
+$categoria->excluir(1);
+$categoria->atualizar(2,"SmartPhone");
+var_dump($categoria->buscarPorId(3));
+var_dump($categoria->buscarTudo());
